fa7bb7506d584baddb2c851a2209ddbc9df90b69
Boy Alone <boyalonetechs@gmail.com>
2025-08-17 21:38:57 +0100

    fix: update favicon link to use custom image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/elvis.jpg" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#0B0B12" />
         <meta
